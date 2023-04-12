1a6ae382c2899f0b2e91eca46931b5ce707d8dc7
AnhTuan1407 <anhtuan2003147@gmail.com>
2023-04-12 14:32:26 +0700

    update

---
+++ Module3/mySQL/ss1_xay_dung_co_so_du_lieu/Bai1.sql
@@ -0,0 +1,26 @@
+create database class;
+
+create table class(
+	id int,
+    name_class varchar(255) 
+);
+
+select * from class;
+insert into class(id, name_class)
+values (1, 'A0922I1'),
+	(2, 'A0123I1');
+
+create table teacher(
+	id int,
+    nameteacher varchar(255),
+    age int,
+    country varchar(255)
+);
+
+select * from teacher;
+insert into teacher (id, nameteacher, age, country)
+value (1, 'Tuan', 20, 'Viet Nam');
+
+drop table class;
+drop table teacher;
+
---
+++ Module3/mySQL/ss5_SQL_view_index_procedure/view_index_storeProcedure_SQL.sql
@@ -0,0 +1,39 @@
+create database product;
+use product;
+create table product (
+	ID  int not null,
+	product_code varchar(50),
+	product_name varchar(50),
+    product_price int not null ,
+    product_amount varchar(50),
+	product_description varchar(50),
+    product_status  bit
+);
+INSERT INTO product
+VALUES (1, 'MG01','May Giat', 300,2,'GOOD',0),
+       (2, 'TL02', 'Tu Lanh', 450,5,'GOOD',1),
+       (3, 'DH03', 'Dieu Hoa', 6000,1,'NORMAL',1),
+       (4, 'QA04', 'Quat', 50,10,'SIMPLE',0),
+       (5, 'BD05', 'Bep Dien',565,4,'OLD',1);
+       
+select * from product;
+
+-- Tạo Unique Index trên bảng Products (sử dụng cột productCode để tạo chỉ mục)
+CREATE UNIQUE INDEX product_code
+ON product (product_code);
+
+-- Tạo Composite Index trên bảng Products (sử dụng 2 cột productName và productPrice)
+CREATE INDEX composite ON product(product_name, product_price);
+
+-- Sử dụng câu lệnh EXPLAIN để biết được câu lệnh SQL của bạn thực thi như nào
+EXPLAIN SELECT * FROM product WHERE product_name = 'Dieu Hoa'; 
+
+create unique index idx_product 
+on product (product_code);
+alter table product add index idx_products(product_name, product_price);
+explain select *from product;
+
+create view product_view as 
+select product_code, product_name, product_price, product_status from product;
+select *from product_view;
+
