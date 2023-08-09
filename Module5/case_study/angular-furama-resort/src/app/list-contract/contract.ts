2feb0c8edc37744dbc2f32d15dff94db112b28a6
AnhTuan1407 <anhtuan2003147@gmail.com>
2023-08-09 18:32:10 +0700

    Update

---
+++ Module5/case_study/angular-furama-resort/src/app/list-contract/contract.ts
@@ -0,0 +1,8 @@
+export interface Contract {
+  customerName?: string;
+  serviceName?: string;
+  contractDayStart?: string;
+  contractDayEnd?: string;
+  contractDeposit?: number;
+  contractTotalCost?: number;
+}
--- Module5/case_study/angular-furama-resort/src/app/list-contract/list-contract.component.ts
+++ Module5/case_study/angular-furama-resort/src/app/list-contract/list-contract.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Contract} from './contract';
 
 @Component({
   selector: 'app-list-contract',
@@ -9,7 +10,41 @@ export class ListContractComponent implements OnInit {
 
   constructor() { }
 
+  contracts: Contract[] = [
+    {
+      customerName: 'Nguyễn Thị Yến',
+      serviceName: 'Room',
+      contractDayStart: '04/02/2002',
+      contractDayEnd: '04/12/2002',
+      contractDeposit: 2.000,
+      contractTotalCost: 4.000
+    },
+    {
+      customerName: 'Nguyễn Phan Anh',
+      serviceName: 'Villa',
+      contractDayStart: '01/02/2004',
+      contractDayEnd: '01/12/2004',
+      contractDeposit: 4.000,
+      contractTotalCost: 16.000
+    },
+    {
+      customerName: 'Trần Thị Mỹ',
+      serviceName: 'House',
+      contractDayStart: '17/10/2003',
+      contractDayEnd: '17/10/2004',
+      contractDeposit: 6.000,
+      contractTotalCost: 24.000
+    },
+    {
+      customerName: 'Phạm Thị Ngọc',
+      serviceName: 'Villa',
+      contractDayStart: '31/08/2003',
+      contractDayEnd: '31/08/2004',
+      contractDeposit: 8.000,
+      contractTotalCost: 32.000
+    },
+  ];
+
   ngOnInit(): void {
   }
-
 }
